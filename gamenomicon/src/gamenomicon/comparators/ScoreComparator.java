b3a28aa2e813b03bbbf8b5035873077fe189e5f6
b4yerl <gcbayerlc@gmail.com>
2023-08-14 08:03:57 -0600

    Fix ScoreComparator
    - Now ScoreComparator returns the descending order
    - Code added for YearComparator

--- gamenomicon/src/gamenomicon/comparators/ScoreComparator.java
+++ gamenomicon/src/gamenomicon/comparators/ScoreComparator.java
@@ -4,8 +4,9 @@ import gamenomicon.entities.Game;
 import java.util.Comparator;
 
 public class ScoreComparator implements Comparator<Game> {
+    @Override
     public int compare(Game g1, Game g2) {
-        int result = Integer.compare(g1.getScore(), g2.getScore());
+        int result = Integer.compare(g2.getScore(), g1.getScore());
 
         if(result == 0) return g1.compareTo(g2);
         return result;
--- gamenomicon/src/gamenomicon/comparators/YearComparator.java
+++ gamenomicon/src/gamenomicon/comparators/YearComparator.java
@@ -4,4 +4,10 @@ import gamenomicon.entities.Game;
 import java.util.Comparator;
 
 public class YearComparator implements Comparator<Game> {
+    @Override
+    public int compare(Game g1, Game g2) {
+        int result = Integer.compare(g1.getPublishingYear(), g2.getPublishingYear());
+        if(result == 0) return g1.compareTo(g2);
+        return result;
+    }
 }
